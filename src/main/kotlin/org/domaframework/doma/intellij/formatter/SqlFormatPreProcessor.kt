932a1fc52d5edb30fb5b6ef2ee7a611c768765c4
xterao <xterao.haruka@contract.dentsusoken.com>
2025-04-03 18:20:47 +0900

    Make sure to end the file with a space and a newline

--- src/main/kotlin/org/domaframework/doma/intellij/formatter/SqlFormatPreProcessor.kt
+++ src/main/kotlin/org/domaframework/doma/intellij/formatter/SqlFormatPreProcessor.kt
@@ -16,6 +16,7 @@
 package org.domaframework.doma.intellij.formatter
 
 import com.intellij.lang.ASTNode
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
@@ -26,6 +27,7 @@ import com.intellij.psi.TokenType
 import com.intellij.psi.impl.source.codeStyle.PreFormatProcessor
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.elementType
+import com.intellij.psi.util.endOffset
 import com.intellij.psi.util.prevLeafs
 import org.domaframework.doma.intellij.psi.SqlBlockComment
 import org.domaframework.doma.intellij.psi.SqlTypes
@@ -55,11 +57,17 @@ class SqlFormatPreProcessor : PreFormatProcessor {
         var index = keywordList.size
         var keywordIndex = replaceKeywordList.size
 
+        // Add a newline to the end of the file
         val documentLastElement = visitor.lastElement
         val documentLastRange = visitor.lastElement?.textRange
-        if (documentLastRange != null && documentLastRange.endOffset <= rangeToReformat.endOffset) {
-            if (documentLastElement !is PsiWhiteSpace || documentLastElement.text?.contains("\n") == false) {
-                document.insertString(documentLastRange.endOffset, "\n")
+        if (documentLastElement != null && documentLastRange != null && documentLastRange.endOffset <= rangeToReformat.endOffset) {
+            if (documentLastElement !is PsiWhiteSpace) {
+                val textEnd = documentLastElement.endOffset
+                document.insertString(textEnd, " \n")
+            } else {
+                val textStart = documentLastElement.startOffset
+                val textEnd = documentLastElement.endOffset
+                document.replaceString(textStart, textEnd, " \n")
             }
         }
 
@@ -68,112 +76,71 @@ class SqlFormatPreProcessor : PreFormatProcessor {
             val textRangeStart = it.startOffset
             val textRangeEnd = textRangeStart + it.text.length
             if (it.elementType != TokenType.WHITE_SPACE) {
+                // Add a newline before any element that needs a newline+indent, without overlapping if there is already a newline
                 index--
                 var newKeyword = getUpperText(it)
                 when (it.elementType) {
                     SqlTypes.KEYWORD -> {
                         keywordIndex--
-                        newKeyword =
-                            if (checkKeywordPrevElement(index, it) &&
-                                SqlKeywordUtil.getIndentType(it.text).isNewLineGroup() ||
-                                it.text.lowercase() == "end" ||
-                                (
-                                    it.text.lowercase() == "as" &&
-                                        createQueryType == CreateQueryType.VIEW
-                                )
-                            ) {
-                                if (SqlKeywordUtil.isSetLineKeyword(
-                                        it.text,
-                                        keywordList[index - 1].text,
-                                    )
-                                ) {
-                                    getUpperText(it)
-                                } else {
-                                    getNewLineString(it)
-                                }
-                            } else {
-                                getUpperText(it)
-                            }
+                        newKeyword = getKeywordNewText(index, it, createQueryType, keywordList)
                     }
+
                     SqlTypes.LEFT_PAREN -> {
                         newKeyword =
                             if (createQueryType == CreateQueryType.TABLE) {
-                                getNewLineString(it)
+                                getNewLineString(it.prevSibling, getUpperText(it))
                             } else {
                                 getUpperText(it)
                             }
                     }
+
                     SqlTypes.RIGHT_PAREN -> {
-                        val prefixElements =
-                            getElementsBeforeKeyword(it.prevLeafs.toList()) { it.elementType == SqlTypes.LEFT_PAREN }
-                        val containsColumnRaw =
-                            prefixElements.findLast { isColumnDefinedRawElementType(it) } != null
-                        newKeyword =
-                            if (createQueryType == CreateQueryType.TABLE) {
-                                if (containsColumnRaw) {
-                                    getNewLineString(it)
-                                } else {
-                                    getUpperText(it)
-                                }
-                            } else {
-                                getUpperText(it)
-                            }
+                        newKeyword = getRightPatternNewText(it, newKeyword, createQueryType)
                     }
 
                     SqlTypes.WORD -> {
-                        var prev = it.prevSibling
-                        var isColumnName = true
-                        while (prev != null && prev.elementType != SqlTypes.LEFT_PAREN && prev.elementType != SqlTypes.COMMA) {
-                            if (prev !is PsiWhiteSpace &&
-                                prev.elementType != SqlTypes.LINE_COMMENT &&
-                                prev.elementType != SqlTypes.BLOCK_COMMENT
-                            ) {
-                                isColumnName =
-                                    prev.elementType == SqlTypes.COMMA ||
-                                    prev.elementType == SqlTypes.LEFT_PAREN
-                                break
-                            }
-                            prev = prev.prevSibling
-                        }
-
-                        newKeyword =
-                            if (createQueryType == CreateQueryType.TABLE && isColumnName) {
-                                getNewLineString(it)
-                            } else {
-                                getUpperText(it)
-                            }
+                        newKeyword = getWordNewText(it, newKeyword, createQueryType)
                     }
+
                     SqlTypes.COMMA -> {
-                        newKeyword = getNewLineString(it)
+                        newKeyword = getNewLineString(it.prevSibling, getUpperText(it))
                     }
                 }
                 document.deleteString(textRangeStart, textRangeEnd)
                 document.insertString(textRangeStart, newKeyword)
             } else {
-                if (keywordIndex < replaceKeywordList.size) {
+                // Remove spaces after newlines to reset indentation
+                val nextSibling = it.nextSibling
+                if (nextSibling.elementType == SqlTypes.LINE_COMMENT) {
+                    document.replaceString(textRangeStart, textRangeEnd, "")
+                } else if (nextSibling.elementType == SqlTypes.BLOCK_COMMENT) {
+                    removeSpacesAroundNewline(document, it.textRange)
+                } else if (keywordIndex < replaceKeywordList.size) {
                     val nextElement = replaceKeywordList[keywordIndex]
-                    if ((
-                            SqlKeywordUtil.getIndentType(nextElement.text ?: "").isNewLineGroup() &&
+                    if (isNewLineOnlyCreateTable(nextSibling) && createQueryType == CreateQueryType.TABLE) {
+                        removeSpacesAroundNewline(document, it.textRange)
+                    } else if (isSubGroupFirstElement(nextElement)) {
+                        document.deleteString(textRangeStart, textRangeEnd)
+                    } else {
+                        val isNewLineGroup = SqlKeywordUtil.getIndentType(nextElement.text ?: "").isNewLineGroup()
+                        val isSetLineKeyword =
+                            if (keywordIndex > 0) {
                                 SqlKeywordUtil.isSetLineKeyword(
-                                    replaceKeywordList[keywordIndex].text,
+                                    nextElement.text,
                                     replaceKeywordList[keywordIndex - 1].text,
                                 )
-                        ) ||
-                        (
-                            isNewLineOnlyCreateTable(nextElement) && createQueryType == CreateQueryType.TABLE
-                        )
-                    ) {
-                        document.deleteString(textRangeStart, textRangeEnd)
-                        document.insertString(textRangeStart, " ")
-                    } else {
-                        val currentIndent = it.text.substringAfter("\n", "").length
-                        val start = textRangeEnd - currentIndent
-                        document.deleteString(start, textRangeEnd)
+                            } else {
+                                false
+                            }
+
+                        if (isNewLineGroup && !isSetLineKeyword || keywordList[index].elementType == SqlTypes.COMMA) {
+                            removeSpacesAroundNewline(document, it.textRange)
+                        } else {
+                            document.replaceString(textRangeStart, textRangeEnd, " ")
+                        }
                     }
                 } else {
-                    val currentIndent = it.text.substringAfter("\n", "").length
-                    val start = textRangeEnd - currentIndent
-                    document.deleteString(start, textRangeEnd)
+                    removeSpacesAroundNewline(document, it.textRange)
                 }
             }
         }
@@ -182,6 +149,103 @@ class SqlFormatPreProcessor : PreFormatProcessor {
 
         return rangeToReformat.grown(visitor.replaces.size)
     }
+
+    private fun removeSpacesAroundNewline(
+        document: Document,
+        range: TextRange,
+    ) {
+        val originalText = document.getText(range)
+        val newText = originalText.replace(Regex("\\s*\\n\\s*"), "\n")
+        document.replaceString(range.startOffset, range.endOffset, newText)
+    }
+
+    /**
+     * Checks for special case keyword elements and specific combinations of keywords with line breaks and capitalization only
+     */
+    private fun getKeywordNewText(
+        index: Int,
+        element: PsiElement,
+        createQueryType: CreateQueryType,
+        keywordList: List<PsiElement>,
+    ): String =
+        if (element.text.lowercase() == "end") {
+            getNewLineString(element.prevSibling, getUpperText(element))
+        } else if (isCreateViewAs(element, createQueryType)) {
+            getNewLineString(element.prevSibling, getUpperText(element))
+        } else if (isSubGroupFirstElement(element)) {
+            getUpperText(element)
+        } else if (SqlKeywordUtil.getIndentType(element.text).isNewLineGroup()) {
+            if (index > 0 &&
+                SqlKeywordUtil.isSetLineKeyword(
+                    element.text,
+                    keywordList[index - 1].text,
+                )
+            ) {
+                getUpperText(element)
+            } else {
+                getNewLineString(element.prevSibling, getUpperText(element))
+            }
+        } else {
+            getUpperText(element)
+        }
+
+    private fun getRightPatternNewText(
+        element: PsiElement,
+        newKeyword: String,
+        createQueryType: CreateQueryType,
+    ): String {
+        var newKeyword1 = newKeyword
+        val prefixElements =
+            getElementsBeforeKeyword(element.prevLeafs.toList()) { it.elementType == SqlTypes.LEFT_PAREN }
+        val containsColumnRaw =
+            prefixElements.findLast { isColumnDefinedRawElementType(it) } != null
+        newKeyword1 =
+            if (createQueryType == CreateQueryType.TABLE) {
+                if (containsColumnRaw) {
+                    getNewLineString(element.prevSibling, getUpperText(element))
+                } else {
+                    getUpperText(element)
+                }
+            } else {
+                getUpperText(element)
+            }
+        return newKeyword1
+    }
+
+    private fun getWordNewText(
+        element: PsiElement,
+        newKeyword: String,
+        createQueryType: CreateQueryType,
+    ): String {
+        newKeyword
+        var prev = element.prevSibling
+        var isColumnName = true
+        while (prev != null && prev.elementType != SqlTypes.LEFT_PAREN && prev.elementType != SqlTypes.COMMA) {
+            if (prev !is PsiWhiteSpace &&
+                prev.elementType != SqlTypes.LINE_COMMENT &&
+                prev.elementType != SqlTypes.BLOCK_COMMENT
+            ) {
+                isColumnName =
+                    prev.elementType == SqlTypes.COMMA ||
+                    prev.elementType == SqlTypes.LEFT_PAREN
+                break
+            }
+            prev = prev.prevSibling
+        }
+
+        return if (createQueryType == CreateQueryType.TABLE && isColumnName) {
+            getNewLineString(element.prevSibling, getUpperText(element))
+        } else {
+            getUpperText(element)
+        }
+    }
+
+    private fun isCreateViewAs(
+        element: PsiElement,
+        createQueryType: CreateQueryType,
+    ): Boolean =
+        element.text.lowercase() == "as" &&
+            createQueryType == CreateQueryType.VIEW
 
     private fun isColumnDefinedRawElementType(element: PsiElement): Boolean =
         element.elementType == SqlTypes.WORD ||
@@ -214,6 +278,9 @@ class SqlFormatPreProcessor : PreFormatProcessor {
         return attachmentKeywordType
     }
 
+    /**
+     * The column definition elements of Create Table, "(", "WORD", and ")" must be line breaks
+     */
     private fun isNewLineOnlyCreateTable(nextElement: PsiElement): Boolean =
         nextElement.elementType == SqlTypes.LEFT_PAREN ||
             nextElement.elementType == SqlTypes.RIGHT_PAREN ||
@@ -224,11 +291,14 @@ class SqlFormatPreProcessor : PreFormatProcessor {
         isLeft: (T) -> Boolean,
     ): List<T> = elements.takeWhile { element -> !isLeft(element) }
 
-    private fun getNewLineString(element: PsiElement): String =
-        if (element.prevSibling?.text?.contains("\n") == false) {
-            "\n${getUpperText(element)}"
+    private fun getNewLineString(
+        prevElement: PsiElement?,
+        text: String,
+    ): String =
+        if (prevElement?.text?.contains("\n") == false) {
+            "\n$text"
         } else {
-            getUpperText(element)
+            text
         }
 
     private fun getUpperText(element: PsiElement): String =
@@ -238,13 +308,9 @@ class SqlFormatPreProcessor : PreFormatProcessor {
             element.text
         }
 
-    private fun checkKeywordPrevElement(
-        index: Int,
-        element: PsiElement,
-    ): Boolean =
-        index > 0 &&
-            element.prevSibling != null &&
-            element.prevSibling.elementType != SqlTypes.LEFT_PAREN
+    private fun isSubGroupFirstElement(element: PsiElement): Boolean =
+        getElementsBeforeKeyword(element.prevLeafs.toList()) { it.elementType == SqlTypes.LEFT_PAREN }
+            .findLast { it !is PsiWhiteSpace } == null
 }
 
 private class SqlFormatVisitor : PsiRecursiveElementVisitor() {
@@ -259,10 +325,7 @@ private class SqlFormatVisitor : PsiRecursiveElementVisitor() {
 
         if (PsiTreeUtil.getParentOfType(element, SqlBlockComment::class.java) == null) {
             when (element.elementType) {
-                SqlTypes.KEYWORD, SqlTypes.COMMA -> {
-                    replaces.add(element)
-                }
-                SqlTypes.LEFT_PAREN, SqlTypes.RIGHT_PAREN, SqlTypes.WORD -> {
+                SqlTypes.KEYWORD, SqlTypes.COMMA, SqlTypes.LEFT_PAREN, SqlTypes.RIGHT_PAREN, SqlTypes.WORD -> {
                     replaces.add(element)
                 }
             }
@@ -273,7 +336,11 @@ private class SqlFormatVisitor : PsiRecursiveElementVisitor() {
         super.visitWhiteSpace(space)
         val nextElement = space.nextSibling
         if (nextElement != null &&
-            space.text.contains("\n")
+            (
+                space.text.contains("\n") ||
+                    nextElement.elementType == SqlTypes.LINE_COMMENT ||
+                    nextElement.elementType == SqlTypes.BLOCK_COMMENT
+            )
         ) {
             replaces.add(space)
         }
