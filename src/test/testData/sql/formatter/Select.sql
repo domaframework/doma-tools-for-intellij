5d6b60b1de01b9c6bf28b166de91f1eae0866fe1
xterao <xterao.haruka@contract.dentsusoken.com>
2025-04-15 10:49:07 +0900

    Add AND and OR groups and conditional comment blocks to Select test cases

--- src/test/testData/sql/formatter/Select.sql
+++ src/test/testData/sql/formatter/Select.sql
@@ -25,14 +25,16 @@ SELECT COUNT( DISTINCT (x)),o.*
                              ON p.objid = s.bestobjid AND p.plate = s.plate
           /** Where */
           WHERE p.TYPE = DBO.FPHOTOTYPE('Star')
-            AND (p.flags & DBO.FPHOTOFLAGS('EDGE')) = 0
+         
+            OR (p.flags & DBO.FPHOTOFLAGS('EDGE') = 0
 
-            AND (p.psfmag_g - p.extinction_g) BETWEEN 15 AND 20
+            AND (p.psfmag_g - p.extinction_g) BETWEEN 15 AND 20)
+/*%if status == 2 */
             AND u.propermotion > 2.
             /** And  Group */
             AND (p.psfmag_g - p.extinction_g + 5 * LOG(u.propermotion / 100.) + 5 > 16.136 + 2.727 * (p.psfmag_g - p.extinction_g - (p.psfmag_i - p.extinction_i))
 
-                  OR p.psfmag_g - p.extinction_g - (p.psfmag_i - p.extinction_i) < 0.) ) AS o
+                  OR p.psfmag_g - p.extinction_g - (p.psfmag_i - p.extinction_i) < 0.)/*%end*/) AS o
 
        LEFT OUTER JOIN ( SELECT n.objid
                                 , MIN(n.distance) AS nearest
@@ -40,18 +42,21 @@ SELECT COUNT( DISTINCT (x)),o.*
                            FROM neighbors n
                                 JOIN phototag x
                                   ON n.neighborobjid = x.objid
-
+                                  AND n.neighbormode = DBO.FPHOTOMODE('Primary')
+                                  OR n.MODE = /*# "active" */'mode'
+                                  AND n.status =2
+                                  AND n.flag = true
                           WHERE n.TYPE = DBO.FPHOTOTYPE('Star')
 
                             AND n.MODE = DBO.FPHOTOMODE('Primary')
-                            AND n.neighbormode = DBO.FPHOTOMODE('Primary')
+                            OR n.neighbormode = DBO.FPHOTOMODE('Primary')
                             AND (x.TYPE = DBO.FPHOTOTYPE('Star')
 
-                                  OR x.TYPE = DBO.FPHOTOTYPE('Galaxy'))
-                            AND x.modelmag_g BETWEEN 10 AND 21
+                                  AND x.TYPE = DBO.FPHOTOTYPE('Galaxy'))
+                            OR x.modelmag_g BETWEEN 10 AND 21
 
                           GROUP BY n.objid ) AS nbor
                     ON o.objid = nbor.objid
-                WHERE p.list IN /* params */(1
+                WHERE p.params IN /* params */(1
                                             ,2
                                             ,3)
