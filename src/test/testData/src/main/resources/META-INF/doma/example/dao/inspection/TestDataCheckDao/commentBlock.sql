cc52630a36ea2a6ff6f68338e13eb18307e70596
xterao <xterao.haruka@contract.dentsusoken.com>
2025-05-12 15:22:52 +0900

    Fixed check for List type test data

--- src/test/testData/src/main/resources/META-INF/doma/example/dao/inspection/TestDataCheckDao/commentBlock.sql
+++ src/test/testData/src/main/resources/META-INF/doma/example/dao/inspection/TestDataCheckDao/commentBlock.sql
@@ -9,4 +9,4 @@ SELECT e.employee_id AS employeeId
  /*%! This comment will be removed */
  WHERE e.employee_id = <error descr="Bind variables must be followed by test data">/*^ id */</error>
    AND e.age >= /*^ literalAge */99
-   AND e.sub_id IN /* subIds */(1,2,3)
+   AND e.sub_id IN /* subIds */(1, 2, 3)
