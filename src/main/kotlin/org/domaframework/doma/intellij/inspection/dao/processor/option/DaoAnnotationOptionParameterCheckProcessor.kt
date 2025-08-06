1b71ea782bab0b3f8248c12a91fc53031a57b885
xterao <xterao.haruka@contract.dentsusoken.com>
2025-08-06 18:04:04 +0900

    Refactor TypeProcessor

--- src/main/kotlin/org/domaframework/doma/intellij/inspection/dao/processor/option/DaoAnnotationOptionParameterCheckProcessor.kt
+++ src/main/kotlin/org/domaframework/doma/intellij/inspection/dao/processor/option/DaoAnnotationOptionParameterCheckProcessor.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright Doma Tools Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.domaframework.doma.intellij.inspection.dao.processor.option
 
 import com.intellij.codeInspection.ProblemsHolder
@@ -52,14 +67,21 @@ class DaoAnnotationOptionParameterCheckProcessor(
             return
         }
 
-        checkArrayOption(annotation, "include", entityClass, holder)
-        checkArrayOption(annotation, "exclude", entityClass, holder)
+        checkAnnotationOptions(annotation, entityClass, holder)
 
         val returningOption = getDaoAnnotationOption(annotation, "returning") ?: return
         if (hasReturingOption()) {
-            checkArrayOption(returningOption, "include", entityClass, holder)
-            checkArrayOption(returningOption, "exclude", entityClass, holder)
+            checkAnnotationOptions(returningOption, entityClass, holder)
         }
+    }
+
+    private fun checkAnnotationOptions(
+        annotation: PsiAnnotation,
+        entityClass: PsiClass,
+        holder: ProblemsHolder,
+    ) {
+        checkArrayOption(annotation, "include", entityClass, holder)
+        checkArrayOption(annotation, "exclude", entityClass, holder)
     }
 
     private fun checkArrayOption(
