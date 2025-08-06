1adf8ecdd5faf3bd56c3e541f839c8c26a0ba155
xterao <xterao.haruka@contract.dentsusoken.com>
2025-08-06 17:47:30 +0900

    Enhance annotation option parameter validation and add new test cases for embedded properties

--- src/test/testData/src/main/java/doma/example/entity/Department.java
+++ src/test/testData/src/main/java/doma/example/entity/Department.java
@@ -1,11 +1,18 @@
 package doma.example.entity;
 
-import org.seasar.doma.Entity;
+import org.seasar.doma.*;
+import doma.example.domain.*;
 
 @Entity
 public class Department {
     public Integer id;
     public String name;
+    @Column(updatable = false)
     public String location;
     public Integer managerCount;
+
+    @Embedded
+    public ClientUser embeddableEntity;
+    @Embedded
+    public ClientUser embeddableEntity2;
 }
