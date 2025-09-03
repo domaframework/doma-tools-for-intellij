3457127e061c4cc8d18c2863e4072bba7de31c34
xterao <xterao.haruka@contract.dentsusoken.com>
2025-09-03 10:25:43 +0900

    Format SQL CASE statements for improved readability and consistency

--- src/test/testData/sql/formatter/SelectCaseEndWithCondition_format.sql
+++ src/test/testData/sql/formatter/SelectCaseEndWithCondition_format.sql
@@ -1,4 +1,12 @@
-SELECT CASE WHEN div = 'A' THEN 'AAA'
+SELECT CASE
+            /*%if isCheckStatus */
+            WHEN status = '1' THEN 1
+            /*%else */
+              /*%if hasPoint */
+              WHEN status = '2' THEN 2
+              /*%end*/
+            /*%end*/
+            WHEN div = 'A' THEN 'AAA'
             /*%if addCondition*/
               /*%if conditionType == 1 */
               WHEN div = 'B' THEN 'BBB1'
@@ -9,3 +17,24 @@ SELECT CASE WHEN div = 'A' THEN 'AAA'
             ELSE 'CCC'
         END AS divName
   FROM users
+ ORDER BY CASE
+               WHEN pickup IS NULL THEN
+                    /*%if isCase*/
+                    CASE
+                         WHEN case_status = 1 THEN 0
+                         ELSE 1
+                     END
+                    /*%elseif isCaseEnd*/
+                    CASE
+                         WHEN case_status = 2 THEN 0
+                         ELSE 1
+                     END
+                    /*%else*/
+                    0
+                    /*%end*/
+               ELSE
+                    CASE
+                         WHEN case_static_status THEN 0
+                         ELSE 1
+                     END
+           END
