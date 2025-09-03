cca6dc5969b308e4d673a0d3178df3b70f606543
xterao <xterao.haruka@contract.dentsusoken.com>
2025-09-03 11:46:03 +0900

    Improve SQL block comment formatting and handling for parser level comments

--- src/test/testData/sql/formatter/BlockCommentParse.sql
+++ src/test/testData/sql/formatter/BlockCommentParse.sql
@@ -1,8 +1,9 @@
-/*%! This is Parser Level Comment*/
+
+ /*%! This is Parser Level Comment*/
 select * from table1
 /*%!This is Parser Level Comment*/
 where column1 = /*# value1*/
 /*+
    * This is Block Comment
    */
-and column2 = /* value2 */ 'value2'     or column3 = /*^ value3 */ 'value3'
+and column2 = /* value2 */ 'value2' /** line break */    or column3 = /*^ value3 */ 'value3'
